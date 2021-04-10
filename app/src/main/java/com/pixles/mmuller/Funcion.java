0dcbf48440d64e7b260d6f97c40080e74533141c
raynor <saibod14@hotmail.com>
2021-04-10 16:10:20 -0500

    terminado

--- app/src/main/java/com/pixles/mmuller/Funcion.java
+++ app/src/main/java/com/pixles/mmuller/Funcion.java
@@ -1,107 +1,39 @@
 package com.pixles.mmuller;
 
 import java.util.*;
+import com.fathzer.soft.javaluator.DoubleEvaluator;
+
 
 public class Funcion {
 
 
     public static double f(String f, double V) {
 
-        int k = 0;
-        String array_f[] = new String[100];
-        double fx[] = new double[100];
-        double Resultado = 0;
-        array_f[0] = "";
+        String funcion="";
 
-        for (int i = 0; i < f.length(); i++) {
-
-            if (!("+".equals(Character.toString((f.charAt(i))))
-                    || "-".equals(Character.toString((f.charAt(i)))))) {
-                array_f[k] = array_f[k] + Character.toString((f.charAt(i)));
-            } else {
-
-                if (i != 0) {
-                    k = k + 1;
-                    array_f[k] = "";
+        for(int i=0;i<f.length();i++){
+            if((f.charAt(i)+"").equals("ˆ")){
+				funcion=funcion+"^";
+            }else {
+                if((f.charAt(i)+"").equals("x")){
+                    funcion=funcion+V;
+                }else{
+                    funcion=funcion+f.charAt(i);
                 }
-                array_f[k] = array_f[k] + Character.toString((f.charAt(i)));
-
             }
         }
 
-        for (int i = 0; i <= k; i++) {
-            for (int j = 0; j < array_f[i].length(); j++) {
-                try {
 
-                    if ("x".equals(Character.toString(array_f[i].charAt(j)))
-                            && exponencial(i, j, array_f[i])) {
-                        double temp_f;
-                        String temp_S = "";
-                        for (int n = (j + 2); n < array_f[i].length(); n++) {
-                            temp_S = temp_S + Character.toString(array_f[i].charAt(n));
-                        }
-                        temp_f = Double.parseDouble(temp_S);
-                        System.out.println(V+" temp_f");
-                        fx[i] = Math.pow(V, temp_f);
 
-                        if (j != 0) {
-                            temp_S = "";
-                            for (int m = 0; m < j; m++) {
-                                temp_S = temp_S + Character
-                                        .toString(array_f[i].charAt(m));
-                            }
-                            temp_f = Double.parseDouble(temp_S);
-                            fx[i] = temp_f * fx[i];
-                        }
+        DoubleEvaluator evaluator = new DoubleEvaluator();
 
-                    }
+        // Evaluate an expression
+        Double resultado = evaluator.evaluate(funcion);
+        // Ouput the result
 
-                    if ("x".equals(Character.toString(array_f[i].charAt(j))) && j != 0
-                            && !(exponencial(i, j, array_f[i]))) {
 
-                        double temp_f;
-                        String temp_S = "";
-                        for (int l = 0; l < j; l++) {
-                            temp_S = temp_S + Character.toString(array_f[i].charAt(l));
-                        }
-                        temp_f = Double.parseDouble(temp_S);
-                        fx[i] = temp_f * V;
-
-                    }
-
-                    if (!(BuscaX(i, j, array_f[i]))) {
-
-                        double temp_f = Double.parseDouble(array_f[i]);
-                        fx[i] = temp_f;
-                    }
-
-                } catch (Exception s) {
-
-                }
-
-            }
-
-            Resultado = Resultado + fx[i];
-        }
-
-        return Resultado;
+        return resultado;
     }
 
-    public static boolean exponencial(int i, int j, String valor_S) {
-        for (int p = j; p < valor_S.length(); p++) {
-            if ("ˆ".equals(Character.toString(valor_S.charAt(p)))) {
-                return true;
-            }
-        }
-        return false;
-    }
 
-    public static boolean BuscaX(int i, int j, String valor_S) {
-        for (int p = j; p < valor_S.length(); p++) {
-            if ("x".equals(Character.toString(valor_S.charAt(p)))) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
--- app/src/main/java/com/pixles/mmuller/MainActivity.java
+++ app/src/main/java/com/pixles/mmuller/MainActivity.java
@@ -22,19 +22,22 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-    static String[][] spaceProbes={
-            {"1","4.5","5.5","5.0","1.0","-0.50","62.25","69.75","15.0","62.25","48.0","3.976","0.61"},
+
+    String [][] Datos={
+            {"","","","","","","","","","","","",""},
 
 
     };
     EditText xx0,xx1,xx2,Funn,pree;
+    TableView<String[]> tableView;
     TextView Raiz;
+    String raz="";
     static String[] spaceProbeHeaders={"#i","X0","X1","X2","h0","h1","&0","&1","a","b","c","X3","Ea"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TableView<String[]> tableView = (TableView<String[]>) findViewById(R.id.table);
+        tableView = (TableView<String[]>) findViewById(R.id.table);
 
         Funn = (EditText)  findViewById(R.id.funcion);
         xx0 = (EditText)  findViewById(R.id.x0);
@@ -43,38 +46,68 @@ public class MainActivity extends AppCompatActivity {
         pree = (EditText)  findViewById(R.id.pre);
         Raiz=(TextView) findViewById(R.id.raiz);
 
-
         tableView.setHeaderBackgroundColor(Color.parseColor("#03A9F4"));
         tableView.setScrollBarSize(10);
 
-        tableView.setMinimumHeight(400);
+        //tableView.setMinimumHeight(300);
 
         tableView.setHeaderAdapter(new SimpleTableHeaderAdapter(this,spaceProbeHeaders));
         tableView.setColumnCount(13);
-        tableView.setDataAdapter(new SimpleTableDataAdapter(MainActivity.this, spaceProbes));
+        tableView.setDataAdapter(new SimpleTableDataAdapter(MainActivity.this, Datos));
 
         tableView.addDataClickListener(new TableDataClickListener() {
             @Override
             public void onDataClicked(int rowIndex, Object clickedData) {
-                Toast.makeText(MainActivity.this, ((String[])clickedData)[1], Toast.LENGTH_SHORT).show();
+               // Toast.makeText(MainActivity.this, ((String[])clickedData)[1], Toast.LENGTH_SHORT).show();
             }
         });
+
+
+        Raiz.setText(""+raz);
+        tableView.setHeaderVisible(false);
+
+        if(Funn.getText().toString().equals("") || xx0.getText().toString().equals("") || xx1.getText().toString().equals("") || xx2.getText().toString().equals("") || pree.getText().toString().equals("")){
+
+        }else{
+            String fun = Funn.getText().toString();
+
+            Double x0 = Double.parseDouble(xx0.getText().toString());
+            Double x1 = Double.parseDouble(xx1.getText().toString());
+            Double x2 = Double.parseDouble(xx2.getText().toString());
+            Double preci = Double.parseDouble(pree.getText().toString());
+
+
+            Mmuller(fun, x0, x1, x2, preci);
+        }
+
+
+
 
     }
 
     public void onclick(View v){
-        String fun=Funn.getText().toString();
 
-        Double x0=Double.parseDouble(xx0.getText().toString());
-        Double x1=Double.parseDouble(xx1.getText().toString());
-        Double x2=Double.parseDouble(xx2.getText().toString());
-        Double preci=Double.parseDouble(pree.getText().toString());
+        if(Funn.getText().toString().equals("") || xx0.getText().toString().equals("") || xx1.getText().toString().equals("") || xx2.getText().toString().equals("") || pree.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(), "Llene todos los Campos",Toast.LENGTH_LONG).show();
+        }else {
 
-        Raiz.setText(""+Mmuller(fun,x0,x1,x2,preci));
+            String fun = Funn.getText().toString();
+
+            Double x0 = Double.parseDouble(xx0.getText().toString());
+            Double x1 = Double.parseDouble(xx1.getText().toString());
+            Double x2 = Double.parseDouble(xx2.getText().toString());
+            Double preci = Double.parseDouble(pree.getText().toString());
+
+
+            Mmuller(fun, x0, x1, x2, preci);
+        }
 
     }
 
-    public double Mmuller(String Fun,double x0,double x1,double x2, double preci){
+    public void Mmuller(String Fun,double x0,double x1,double x2, double preci){
+
+        double x00=x0,x11=x1,x22=x2;
+
         double error =0;
         double x3=0;
         double c=0;
@@ -84,40 +117,118 @@ public class MainActivity extends AppCompatActivity {
         double h1=0;
         double I0=0;
         double I1=0;
-       // while (error>preci){
-            double fx0=Funcion.f(Fun,x0);
-            double fx1=Funcion.f(Fun,x1);
-            double fx2=Funcion.f(Fun,x2);
-            System.out.println("fx0: "+fx0+" fx1:"+fx1+" fx2: "+fx2);
-            h0=x1-x0;
-            h1=x2-x1;
+        int intar=0;
+        do {
+            double fx0 = Funcion.f(Fun, x0);
+            double fx1 = Funcion.f(Fun, x1);
+            double fx2 = Funcion.f(Fun, x2);
+            System.out.println("fx0: " + fx0 + " fx1:" + fx1 + " fx2: " + fx2);
+            h0 = x1 - x0;
+            h1 = x2 - x1;
 
-            I0=(fx1-fx0)/(x1-x0);
-            I1=(fx2-fx1)/(x2-x1);
+            I0 = (fx1 - fx0) / (x1 - x0);
+            I1 = (fx2 - fx1) / (x2 - x1);
 
-            a=(I1-I0)/(h1+h0);
-            b=(a*h1)+I1;
-            c=fx2;
+            a = (I1 - I0) / (h1 + h0);
+            b = (a * h1) + I1;
+            c = fx2;
 
-            System.out.println("h0: "+h0+" h1: "+h1+" I0: "+I0+" I1: "+I1+" a: "+a+" b: "+b+" c: "+c);
-
-            double raiz=Math.sqrt((Math.pow(b,2))-4*a*c);
-            if((Math.abs(b+raiz))>(Math.abs(b-raiz))){
-                x3=x2+((-2*c)/(b+raiz));
-            }else{
-                x3=x2+((-2*c)/(b-raiz));
+            double raiz = Math.sqrt((Math.pow(b, 2)) - 4 * a * c);
+            if ((Math.abs(b + raiz)) > (Math.abs(b - raiz))) {
+                x3 = x2 + ((-2 * c) / (b + raiz));
+            } else {
+                x3 = x2 + ((-2 * c) / (b - raiz));
             }
 
-            error=Math.abs((x3-x2)/(x3))*100;
+            error = Math.abs((x3 - x2) / (x3)) * 100;
 
-            x0=x1;
-            x1=x2;
-            x2=x3;
+            x0 = x1;
+            x1 = x2;
+            x2 = x3;
+            intar++;
+        }while(error > preci);
+
+        Datos= new String[intar][13];
+        error =0;
+         x3=0;
+         c=0;
+         b=0;
+         a=0;
+         h0=0;
+         h1=0;
+         I0=0;
+         I1=0;
+         x0=x00;
+         x1=x11;
+         x2=x22;
+         int regu=0;
+        do {
+            double fx0 = Funcion.f(Fun, x0);
+            double fx1 = Funcion.f(Fun, x1);
+            double fx2 = Funcion.f(Fun, x2);
+
+            h0 = x1 - x0;
+            h1 = x2 - x1;
+
+            I0 = (fx1 - fx0) / (x1 - x0);
+            I1 = (fx2 - fx1) / (x2 - x1);
+
+            a = (I1 - I0) / (h1 + h0);
+            b = (a * h1) + I1;
+            c = fx2;
 
 
-            
-       // }
-        return x3;
+            double raiz = Math.sqrt((Math.pow(b, 2)) - 4 * a * c);
+            if ((Math.abs(b + raiz)) > (Math.abs(b - raiz))) {
+                x3 = x2 + ((-2 * c) / (b + raiz));
+            } else {
+                x3 = x2 + ((-2 * c) / (b - raiz));
+            }
+
+            error = Math.abs((x3 - x2) / (x3)) * 100;
+
+            Datos[regu][0]=(regu+1)+"";
+            Datos[regu][1]=""+x0;
+            Datos[regu][2]=""+x1;
+            Datos[regu][3]=""+x2;
+            Datos[regu][4]=""+h0;
+            Datos[regu][5]=""+h1;
+            Datos[regu][6]=""+I0;
+            Datos[regu][7]=""+I1;
+            Datos[regu][8]=""+a;
+            Datos[regu][9]=""+b;
+            Datos[regu][10]=""+c;
+            Datos[regu][11]=""+x3;
+            Datos[regu][12]=""+error;
+
+            x0 = x1;
+            x1 = x2;
+            x2 = x3;
+            regu++;
+        }while(error > preci);
+
+
+        tableView.setHeaderVisible(true);
+        tableView.setHeaderBackgroundColor(Color.parseColor("#03A9F4"));
+        tableView.setScrollBarSize(10);
+
+        //tableView.setMinimumHeight(300);
+        //static String[] spaceProbeHeaders={"#i","X0","X1","X2","h0","h1","&0","&1","a","b","c","X3","Ea"};
+        tableView.setHeaderAdapter(new SimpleTableHeaderAdapter(this,spaceProbeHeaders));
+        tableView.setColumnCount(13);
+        tableView.setDataAdapter(new SimpleTableDataAdapter(MainActivity.this, Datos));
+
+        tableView.addDataClickListener(new TableDataClickListener() {
+            @Override
+            public void onDataClicked(int rowIndex, Object clickedData) {
+                String text="#i: "+((String[])clickedData)[0]+"\n"+"X0: "+((String[])clickedData)[1]+"\n"+"X1: "+((String[])clickedData)[2]+"\n"+"X2: "+((String[])clickedData)[3]+"\n"+"h0: "+((String[])clickedData)[4]+"\n"+"h1: "+((String[])clickedData)[5]+"\n"+"&0: "+((String[])clickedData)[6]
+                        +"\n"+"&1: "+((String[])clickedData)[7]+"\n"+"a: "+((String[])clickedData)[8]+"\n"+"b: "+((String[])clickedData)[9]+"\n"+"c: "+((String[])clickedData)[10]+"\n"+"X3: "+((String[])clickedData)[11]+"\n"+"Ea: "+((String[])clickedData)[12];
+                Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        raz=Datos[intar-1][11];
+        Raiz.setText(""+raz);
 
 
     }
